62f08a7b9a9bf67cf27c75d810e1c4cc73f6a5d2
Wotw-Potp <s.otsuka08@gmail.com>
2025-06-10 13:57:29 +0900

    feat: meta関連データを追加

--- project/src/libs/constants.ts
+++ project/src/libs/constants.ts
@@ -1,3 +1,14 @@
+// Constants for the project
+// Project Name
+export const PROJECT_NAME =
+  import.meta.env.VITE_PROJECT_NAME || '手が汚れない！インクブレンダー'
+// Project Description
+export const PROJECT_DESCRIPTION =
+  import.meta.env.VITE_PROJECT_DESCRIPTION ||
+  '手が汚れない！インクブレンダーは、手を汚さずにインクを混ぜることができるアプリです。自分だけのインクを作ろう！'
+// Project Base URL
+export const PROJECT_BASE_URL =
+  import.meta.env.VITE_PROJECT_BASE_URL || 'http://localhost:3000'
 // Maximum number of selectable ink colors
 export const MAX_INK_COUNT = 4
 // Maximum number of mixable drops
